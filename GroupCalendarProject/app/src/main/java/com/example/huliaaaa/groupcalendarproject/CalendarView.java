be7fd8ba9ca0ccbe79acddf02fef98ca207ff5e5
sdfs <fsdf>
2017-10-16 17:39:29 -0400

    changed to time picker and date picker

--- GroupCalendarProject/app/src/main/java/com/example/huliaaaa/groupcalendarproject/CalendarView.java
+++ GroupCalendarProject/app/src/main/java/com/example/huliaaaa/groupcalendarproject/CalendarView.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.icu.text.DateFormat;
 import android.icu.text.SimpleDateFormat;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +15,13 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
@@ -62,6 +67,19 @@ public class CalendarView extends AppCompatActivity
     Button exit;
     long m = System.currentTimeMillis();
     Date p = new Date(m);
+    Button delete;
+    CheckBox ok;
+    CheckBox no;
+    TextView ays;
+    RadioButton pickDate;
+    RadioButton pickTime;
+    TimePicker tp;
+    DatePicker dp;
+    Button okay;
+    Button okay2;
+    String thedate;
+    EditText time;
+
 
 
     private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM- yyyy", Locale.getDefault());
@@ -79,7 +97,7 @@ public class CalendarView extends AppCompatActivity
         compactCalendar = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
         compactCalendar.setUseThreeLetterAbbreviation(true);
         title = (EditText) findViewById(R.id.titleTXT);
-        d = (EditText) findViewById(R.id.dateTXT);
+       // d = (EditText) findViewById(R.id.dateTXT);
         description = (EditText) findViewById(R.id.descriptionTXT);
         b2 = (Button) findViewById(R.id.createEvent);
         b3 = (Button) findViewById(R.id.addBTN);
@@ -90,9 +108,21 @@ public class CalendarView extends AppCompatActivity
         editBtn = (Button) findViewById(R.id.editBtn);
         tv1 = (TextView) findViewById(R.id.textView10);
         tv2 = (TextView) findViewById(R.id.textView14);
-        ex1 = (TextView) findViewById(R.id.dateExample);
-        ex2 = (TextView) findViewById(R.id.dateExample2);
+
+
         exit = (Button) findViewById(R.id.exit);
+        delete = (Button) findViewById(R.id.deleteBTN);
+        ok = (CheckBox) findViewById(R.id.yesBTN);
+        no = (CheckBox) findViewById(R.id.noBtn);
+        ays = (TextView) findViewById(R.id.confirmTXT);
+        pickDate = (RadioButton) findViewById(R.id.pickDateBTN);
+        pickTime = (RadioButton) findViewById(R.id.pickTimeBTN);
+        tp = (TimePicker) findViewById(R.id.timePicker);
+        dp = (DatePicker) findViewById(R.id.datePicker);
+        okay = (Button) findViewById(R.id.okBTN);
+        okay2 = (Button) findViewById(R.id.ok2BTN);
+        time = (EditText) findViewById(R.id.timeTXT);
+
 
 
 
@@ -137,7 +167,61 @@ public class CalendarView extends AppCompatActivity
                             eventTitle.setText(oe.getData().toString());
                             eventDes.setText(oe.getDescription().toString());
                             eventDT.setText(df.format(nd));
+                            delete.setVisibility(View.VISIBLE);
 
+                            delete.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ok.setVisibility(View.VISIBLE);
+                                    ays.setVisibility(View.VISIBLE);
+                                    no.setVisibility(View.VISIBLE);
+                                    editBtn.setVisibility(View.INVISIBLE);
+                                    eventTitle.setVisibility(View.INVISIBLE);
+                                    eventDT.setVisibility(View.INVISIBLE);
+                                    eventDes.setVisibility(View.INVISIBLE);
+                                    tv1.setVisibility(View.INVISIBLE);
+                                    tv2.setVisibility(View.INVISIBLE);
+                                    exit.setVisibility(View.INVISIBLE);
+
+                                        ok.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+
+                                        compactCalendar.setVisibility(View.VISIBLE);
+                                        b2.setVisibility(View.VISIBLE);
+                                        compactCalendar.removeEvents(oe.getTimeInMillis());
+                                                ok.setVisibility(View.INVISIBLE);
+                                                ays.setVisibility(View.INVISIBLE);
+                                                no.setVisibility(View.INVISIBLE);
+                                                ok.setChecked(false);
+                                                delete.setVisibility(View.INVISIBLE);
+                                            }
+                                        });
+
+
+
+                                        no.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+
+                                        ok.setVisibility(View.INVISIBLE);
+                                        ays.setVisibility(View.INVISIBLE);
+                                        no.setVisibility(View.INVISIBLE);
+                                        editBtn.setVisibility(View.VISIBLE);
+                                        eventTitle.setVisibility(View.VISIBLE);
+                                        eventDT.setVisibility(View.VISIBLE);
+                                        eventDes.setVisibility(View.VISIBLE);
+                                        tv1.setVisibility(View.VISIBLE);
+                                        tv2.setVisibility(View.VISIBLE);
+                                        exit.setVisibility(View.VISIBLE);
+                                                delete.setVisibility(View.INVISIBLE);
+                                                no.setChecked(false);
+                                            }
+                                        });
+
+
+                                }
+                            });
 
 
 
@@ -146,6 +230,8 @@ public class CalendarView extends AppCompatActivity
 
 
                 }
+
+
             }
         });
 
@@ -170,17 +256,9 @@ public class CalendarView extends AppCompatActivity
                         adapter1.notifyDataSetChanged();
                     }
                     listView1.setVisibility(View.VISIBLE);
+
                 }
 
-                 // if (dateClicked.getDate() == events[i].getTimeInMillis() && events[i] != null) {
-                 //     Toast.makeText(context, events[i].getData().toString(), Toast.LENGTH_SHORT).show();
-                 // }else {
-
-                 //     Toast.makeText(context, "lol", Toast.LENGTH_SHORT).show();
-                 // }
-                   // d.setText(dateClicked.toString());
-
-               // d.requestFocus();
 
 
 
@@ -196,6 +274,17 @@ public class CalendarView extends AppCompatActivity
         onBtnClick3();
         onExitClick();
     }
+    public void onDeleteClick()
+    {
+        delete.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
+    }
 
     public void onBtnClick2()
     {
@@ -205,14 +294,141 @@ public class CalendarView extends AppCompatActivity
             public void onClick(View v)
             {
                 description.setVisibility(View.VISIBLE);
-                d.setVisibility(View.VISIBLE);
+               // d.setVisibility(View.VISIBLE);
                 listView1.setVisibility(View.INVISIBLE);
+                b2.setVisibility(View.INVISIBLE);
 
                 title.setVisibility(View.VISIBLE);
                 b3.setVisibility(View.VISIBLE);
 
-                ex1.setVisibility(View.VISIBLE);
-                ex2.setVisibility(View.VISIBLE);
+               // ex1.setVisibility(View.VISIBLE);
+               // ex2.setVisibility(View.VISIBLE);
+                pickDate.setVisibility(View.VISIBLE);
+                pickTime.setVisibility(View.VISIBLE);
+
+                pickDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pickDate.setChecked(true);
+                        dp.setVisibility(View.VISIBLE);
+                        description.setVisibility(View.INVISIBLE);
+                        //d.setVisibility(View.INVISIBLE);
+                        listView1.setVisibility(View.INVISIBLE);
+                        b2.setVisibility(View.INVISIBLE);
+
+                        title.setVisibility(View.INVISIBLE);
+                        b3.setVisibility(View.INVISIBLE);
+
+
+                        pickDate.setVisibility(View.INVISIBLE);
+                        pickTime.setVisibility(View.INVISIBLE);
+                        compactCalendar.setVisibility(View.INVISIBLE);
+                        okay.setVisibility(View.VISIBLE);
+
+                        okay.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dp.setVisibility(View.INVISIBLE);
+                                description.setVisibility(View.VISIBLE);
+                              //  d.setVisibility(View.VISIBLE);
+                                listView1.setVisibility(View.VISIBLE);
+                                b2.setVisibility(View.VISIBLE);
+
+                                title.setVisibility(View.VISIBLE);
+                                b3.setVisibility(View.VISIBLE);
+
+                                //ex1.setVisibility(View.VISIBLE);
+                                //ex2.setVisibility(View.VISIBLE);
+                                pickDate.setVisibility(View.VISIBLE);
+                                pickTime.setVisibility(View.VISIBLE);
+                                compactCalendar.setVisibility(View.VISIBLE);
+                                okay.setVisibility(View.INVISIBLE);
+                              //  dp.setMinDate(System.currentTimeMillis());
+
+                              int z = dp.getDayOfMonth();
+                              int za = dp.getMonth();
+                              int zq = dp.getYear();
+                             String zz = String.valueOf(z);
+                             String zaa =String.valueOf(za);
+                             String zqq =String.valueOf(zq);
+                             thedate = zaa+ "/"  + zz+ "/" + zqq;
+                             //   d.setText(thedate);
+
+
+
+                               // Date date = new Date(q);
+                                //long milliseconds = z+za+zq;
+
+
+                           // nd = new Date(dp.getYear(),
+                           // dp.getMonth(),
+                           // dp.getDayOfMonth());
+
+                           // //doesnt actually format
+                           // DateFormat fd = new SimpleDateFormat("MM/dd/yy");
+
+
+                           // //formatted value of current Date
+                           // fd.format(nd);
+                           // d.setText(nd.toString());
+
+                            }
+                        });
+
+
+                    }
+                });
+                pickTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pickTime.setChecked(true);
+                        tp.setVisibility(View.VISIBLE);
+                        description.setVisibility(View.INVISIBLE);
+                       // d.setVisibility(View.INVISIBLE);
+                        listView1.setVisibility(View.INVISIBLE);
+                        b2.setVisibility(View.INVISIBLE);
+
+                        title.setVisibility(View.INVISIBLE);
+                        b3.setVisibility(View.INVISIBLE);
+
+                        pickDate.setVisibility(View.INVISIBLE);
+                        pickTime.setVisibility(View.INVISIBLE);
+                        compactCalendar.setVisibility(View.INVISIBLE);
+                        okay2.setVisibility(View.VISIBLE);
+
+                        okay2.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                tp.setVisibility(View.INVISIBLE);
+                                description.setVisibility(View.VISIBLE);
+                                //d.setVisibility(View.VISIBLE);
+                                listView1.setVisibility(View.VISIBLE);
+                                b2.setVisibility(View.VISIBLE);
+                               // time.setVisibility(View.VISIBLE);
+
+                                title.setVisibility(View.VISIBLE);
+                                b3.setVisibility(View.VISIBLE);
+
+
+                                pickDate.setVisibility(View.VISIBLE);
+                                pickTime.setVisibility(View.VISIBLE);
+                                compactCalendar.setVisibility(View.VISIBLE);
+                                okay2.setVisibility(View.INVISIBLE);
+
+                                int hour = tp.getHour();
+                                int minute =  tp.getMinute();
+
+                                String thehour = String.valueOf(hour);
+                                String theminute = String.valueOf(minute);
+
+
+                              //  time.setText(thehour + ":" + theminute);
+
+                            }
+                        });
+                    }
+                });
+
 
                 title.requestFocus();
 
@@ -254,7 +470,7 @@ public void onExitClick()
                 String des = description.getText().toString();
                // View focusView = null;
                 String tit = title.getText().toString();
-                String dat = d.getText().toString();
+              //  String dat = d.getText().toString();
 
 
                 if (TextUtils.isEmpty(des))
@@ -266,14 +482,22 @@ public void onExitClick()
                 {
                     title.setError(getString(R.string.error_field_required));
                 }
-                else if (TextUtils.isEmpty(dat))
-                {
-                    d.setError(getString(R.string.error_field_required));
-                }
+             // else if (TextUtils.isEmpty(dat))
+             // {
+             //     d.setError(getString(R.string.error_field_required));
+             // }
                 else
                 {
-                    String q = d.getText().toString();
-                    Date date = new Date(q);
+               // int z = dp.getDayOfMonth();
+               // int za = dp.getMonth();
+               // int zq = dp.getYear();
+               // String zz = Integer.toString(z);
+               // String zaa = Integer.toString(za);
+               // String zqq = Integer.toString(zq);
+               // String q = zz + zaa + zqq;
+               // Date date = new Date(q);
+               // long milliseconds = date.getTime();
+                    Date date = new Date(dp.getYear()-1900, dp.getMonth(), dp.getDayOfMonth(), tp.getHour(), tp.getMinute());
                     long milliseconds = date.getTime();
 
 
@@ -283,15 +507,16 @@ public void onExitClick()
                     Event event = new Event(Color.RED, milliseconds,title.getText().toString() );
                     compactCalendar.addEvent(event);
                     description.setVisibility(View.INVISIBLE);
-                    d.setVisibility(View.INVISIBLE);
+                   // d.setVisibility(View.INVISIBLE);
                     title.setVisibility(View.INVISIBLE);
                     b3.setVisibility(View.INVISIBLE);
-                    ex1.setVisibility(View.INVISIBLE);
-                    ex2.setVisibility(View.INVISIBLE);
+                    pickDate.setVisibility(View.INVISIBLE);
+                    pickTime.setVisibility(View.INVISIBLE);
+
 
 
                     description.setText("");
-                    d.setText("");
+                  //  d.setText("");
                     title.setText("");
                     compactCalendar.setCurrentDate(p);
                 }
